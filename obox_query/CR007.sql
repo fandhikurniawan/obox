98da552b991400b9d75f97fac13c7410ac99bf40
puguhjayadi <puguh.jayadi@gmail.com>
2021-11-27 08:31:36 +0700

    update CR007

--- obox_query/CR007.sql
+++ obox_query/CR007.sql
@@ -1,12 +1,12 @@
 SELECT 
 'D' as FlagDetail, sk.SandiKantor, d.CabangEntry, rn.Nama as NamaDebitur, d.Kode as NoCIF, rn.KTP as NoIdentitas, d.GolonganKredit KodeKelompokKredit, d.Rekening, d.`GolonganDebiturLBBPR` JenisKredit,
 DATE_FORMAT(ad.tgl, '%Y%m%d') as TanggalTopUp,
-(ang.`DPokok` - ang.`KPokok`) as PlafonSebelum,
-(ang.`DPokok` - ang.`KPokok` + ad.`Plafond`) as PlafonSesudah,
-(ang.`DPokok` - ang.`KPokok` + ad.`Plafond`) - (ang.`DPokok` - ang.`KPokok`) as SelisihPlafon,
-(d.`Plafond` - ang.`KPokok`) as BakiDebetSebelum,
-(d.`Plafond` - ang.`KPokok` + ad.`Plafond`) as BakiDebetSesudah,
-(d.`Plafond` - ang.`KPokok` + ad.`Plafond`) - (d.`Plafond` - ang.`KPokok`) as SelisihBakiDebet,
+@PlafonSebelum := IF(ang.`DPokok` - ang.`KPokok` <= 0, 0, (ang.`DPokok` - ang.`KPokok`)) as PlafonSebelum,
+@PlafonSesudah := IF(@PlafonSebelum + ad.`Plafond` <= 0, 0, (@PlafonSebelum + ad.`Plafond`)) as PlafonSesudah,
+(@PlafonSesudah - @PlafonSebelum) as SelisihPlafon,
+@BakiDebetSebelum := IF(ang.`DPokok` - ang.`KPokok` <= 0, 0, (ang.`DPokok` - ang.`KPokok`)) as BakiDebetSebelum,
+@BakiDebetSesudah := IF(@BakiDebetSebelum + ad.`Plafond` <= 0, 0, (@BakiDebetSebelum + ad.`Plafond`)) as BakiDebetSesudah,
+(@BakiDebetSesudah - @BakiDebetSebelum) as SelisihBakiDebet,
 1 as JumlahRekeningSebelum,
 1 as JumlahRekeningSesudah
 FROM 
@@ -17,6 +17,6 @@ JOIN angsuran ang ON ang.Rekening = d.rekening
 JOIN sandikantor sk ON sk.CabangEntry = d.CabangEntry 
 WHERE DATE_ADD(d.tgl, INTERVAL d.lama MONTH) BETWEEN DATE_SUB(curdate(), INTERVAL 2 WEEK) AND curdate()
 AND sk.SandiKantor IN (001, 002)
--- GROUP BY d.rekening
+AND (ang.`DPokok` - ang.`KPokok`) > 0
 ORDER BY d.rekening DESC
 LIMIT 20;
